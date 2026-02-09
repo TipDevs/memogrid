64e4619e8a4b00d46caf894153a5939349de29ff
TipDevs <tipdevs@gmail.com>
2026-02-10 00:01:54 +0100

    feat(ui): add Loader component

---
+++ src/Components/Loader/loader.jsx
@@ -0,0 +1,12 @@
+function Loader() {
+  return (
+    <button type="button" className="flex justify-center items-center text-white font-bold px-4 py-2 rounded-lg disabled:opacity-50" disabled>
+        <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"></path>
+        </svg>
+      Processing…
+    </button>
+  );
+}
+export default Loader;

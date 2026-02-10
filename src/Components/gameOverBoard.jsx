19e3fa95b6aee0473fc31b126e109c69df67bca3
TipDevs <tipdevs@gmail.com>
2026-02-10 21:46:42 +0100

    feat(style): add new style for a better UI

--- src/Components/gameOverBoard.jsx
+++ src/Components/gameOverBoard.jsx
@@ -3,17 +3,19 @@ function GameOverBoard(props) {
     <>
       <div
         className={`flex flex-col w-[80%] max-w-[450px] items-center justify-center bg-[#ffffff00] rounded-lg backdrop-blur-2xl shadow-md p-[10px] animate-[fadeIn_1s_ease-in-out]`}>
-        {props.cacheData.every((data) => data.selected === true) && (
-          <p className="text-white">
+        {props.allSelected && (
+          <p className="text-white flex flex-col items-center">
             Game Over!!! <span>You successfully pass the game ❤</span>
           </p>
         )}
-        {props.cardClickedTwice ? (
-          <p className="text-white">
+        {props.cardClickedTwice && (
+          <p className="text-[#ff0000] flex flex-col items-center">
             Game Over!!! <span>You clicked twice, try again 😔</span>
           </p>
-        ) : (
-          <p className="text-white">
+        )}
+
+        {!props.cardClickedTwice && !props.allSelected && (
+          <p className="text-[#ff0000] flex flex-col items-center">
             Game Over!!! <span>Time Up 😢</span>
           </p>
         )}
